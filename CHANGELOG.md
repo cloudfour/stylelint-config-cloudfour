562d03ce3f1bcec992305a434a7b1884f4b66f5b
Scott Vandehey <scott@cloudfour.com>
2021-11-15 09:48:37 -0800

    Upgrade to Stylelint v14
    This PR updates the config to Stylelint v14, and also updates the repo
    to follow the Stylelint standard from stylelint-config-recommended.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,56 +1,61 @@
+# Unreleased
+
+- Major version bump due to dependencies
+- Updated `stylelint` to v14.0.0
+
 # 4.0.0 - 2021-02-22
 
--   Updated to v15 of `stylelint-config-suitcss`, which updates its peer
-    dependency for `stylelint` to v13.
+- Updated to v15 of `stylelint-config-suitcss`, which updates its peer
+  dependency for `stylelint` to v13.
 
 # 3.1.2 - 2020-03-06
 
--   Fix another typo in README. (version bump to deploy to NPM)
+- Fix another typo in README. (version bump to deploy to NPM)
 
 # 3.1.1 - 2020-03-06
 
--   Fix typo in README. (version bump to deploy to NPM)
+- Fix typo in README. (version bump to deploy to NPM)
 
 # 3.1.0 - 2020-03-06
 
--   We no longer enforce blank lines before rules after comments.
--   We no longer enforce empty lines before at-rules.
+- We no longer enforce blank lines before rules after comments.
+- We no longer enforce empty lines before at-rules.
 
 # 3.0.0 - 2020-02-07
 
--   Major version bump due dependencies
--   Updated `stylelint` to v13
--   Updated `jest` to v25
--   Updated `eslint` to v6.8.0
+- Major version bump due dependencies
+- Updated `stylelint` to v13
+- Updated `jest` to v25
+- Updated `eslint` to v6.8.0
 
 # 2.0.0 - 2019-11-19
 
--   Major version bump due to Stylelint v12
+- Major version bump due to Stylelint v12
 
 # 1.0.6 - 2019-02-15
 
--   Updated readme and fixed typo in `.travis.yml`
+- Updated readme and fixed typo in `.travis.yml`
 
 # 1.0.5 - 2019-02-14
 
--   Pin Dependencies
+- Pin Dependencies
 
 # 1.0.4 - 2019-02-14
 
--   Add Renovate
+- Add Renovate
 
 # 1.0.3 - 2019-02-14
 
--   Updated readme
+- Updated readme
 
 # 1.0.2 - 2019-02-14
 
--   Add TravisCI
+- Add TravisCI
 
 # 1.0.1 - 2019-02-14
 
--   Updated readme
+- Updated readme
 
 # 1.0.0 - 2019-02-12
 
--   Initial release
+- Initial release
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,70 +1,73 @@
-"use strict";
+'use strict';
 
-const config = require("../");
-const fs = require("fs");
-const stylelint = require("stylelint");
+const config = require('../');
+const fs = require('fs');
+const stylelint = require('stylelint');
 
-const validCss = fs.readFileSync("./__tests__/css-valid.css", "utf-8");
-const invalidCss = fs.readFileSync("./__tests__/css-invalid.css", "utf-8");
+const validCss = fs.readFileSync('./__tests__/valid.css', 'utf-8');
+const invalidCss = fs.readFileSync('./__tests__/invalid.css', 'utf-8');
 
-describe("flags no warnings with valid css", () => {
-  let result;
+describe('flags no warnings with valid css', () => {
+	let result;
 
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: validCss,
-      config
-    });
-  });
+	beforeEach(() => {
+		result = stylelint.lint({
+			code: validCss,
+			config,
+		});
+	});
 
-  it("did not error", () => {
-    return result.then(data => expect(data.errored).toBeFalsy());
-  });
+	it('did not error', () => {
+		return result.then((data) => expect(data.errored).toBeFalsy());
+	});
 
-  it("flags no warnings", () => {
-    return result.then(data => expect(data.results[0].warnings).toHaveLength(0));
-  });
+	it('flags no warnings', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(0));
+	});
 });
 
-describe("flags warnings with invalid css", () => {
-  let result;
 
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: invalidCss,
-      config
-    });
-  });
+describe('flags warnings with invalid css', () => {
+	let result;
 
-  it("did error", () => {
-    return result.then(data => expect(data.errored).toBeTruthy());
-  });
+	beforeEach(() => {
+		result = stylelint.lint({
+			code: invalidCss,
+			config,
+		});
+	});
 
-  it("flags one warning", () => {
-    return result.then(data => expect(data.results[0].warnings).toHaveLength(1));
-  });
+	it('did error', () => {
+		return result.then((data) => expect(data.errored).toBeTruthy());
+	});
 
-  it("correct warning text", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].text).toBe("Expected a leading zero (number-leading-zero)")
-    );
-  });
+	it('flags one warning', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(1));
+	});
 
-  it("correct rule flagged", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].rule).toBe("number-leading-zero")
-    );
-  });
+	it('correct warning text', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[0].text).toBe(
+				'Expected a leading zero (number-leading-zero)',
+			),
+		);
+	});
 
-  it("correct severity flagged", () => {
-    return result.then(data => expect(data.results[0].warnings[0].severity).toBe("error"));
-  });
+	it('correct rule flagged', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[0].rule).toBe('number-leading-zero'),
+		);
+	});
 
-  it("correct line number", () => {
-    return result.then(data => expect(data.results[0].warnings[0].line).toBe(2));
-  });
+	it('correct severity flagged', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].severity).toBe('error'));
+	});
 
-  it("correct column number", () => {
-    return result.then(data => expect(data.results[0].warnings[0].column).toBe(8));
-  });
+	it('correct line number', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].line).toBe(2));
+	});
+
+	it('correct column number', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].column).toBe(8));
+	});
 });
